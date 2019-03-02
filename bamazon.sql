c901afcf024a9ce92375993d1f20024d98570acc
moonlagoon <courtneyroutsong@gmail.com>
2019-03-02 09:40:18 -0500

    almost finished

--- bamazon.sql
+++ bamazon.sql
@@ -1,25 +1,25 @@
-CREATE DATABASE Bamazon;
+CREATE DATABASE bamazon;
 
-USE Bamazon;
+USE bamazon;
 
 CREATE TABLE products (
-	itemID INT NOT NULL AUTO_INCREMENT,
-    productName VARCHAR(45) NULL,
-    departmentName VARCHAR(45) NULL,
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(45) NULL,
+    department VARCHAR(45) NULL,
     price DECIMAL(10,2) NULL,
-    stockQuantity INT NULL,
-    PRIMARY KEY (`itemID`)
+    stock_quantity INT NULL,
+    PRIMARY KEY (`item_id`)
 );
 
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Cutting Board', 'Kitchen', 13.99, 23);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Sticky Notes', 'Office Products', 9.00, 84);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Nintendo 3DS', 'Electronics', 210.99, 105);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Apple Watch', 'Electronics', 668.95, 32);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Pokemon Cards', 'Toys and Games', 4.49, 243);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Sapphire Necklace', 'Jewerly', 596.99, 38, 2, 50);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('The Moon & Antarctica', 'CD and Vinyl', 24.92, 45);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Volley Ball Net', 'Sports & Outdoors', 39.99, 50);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Monopoly', 'Toys and Games', 18.44, 450);
-INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Rustic Gray Dining Set', 'Home and Kitchen', 824.99, 60);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Cutting Board', 'Kitchen', 1399, 23);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Sticky Notes', 'Office Products', 9.00, 84);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Nintendo 3DS', 'Electronics', 210.99, 105);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Apple Watch', 'Electronics', 668.95, 32);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Pokemon Cards', 'Toys and Games', 4.49, 243);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Sapphire Necklace', 'Jewerly', 596.99, 50);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('The Moon & Antarctica', 'CD and Vinyl', 24.92, 45);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Volley Ball Net', 'Sports & Outdoors', 39.99, 50);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Monopoly', 'Toys and Games', 18.44, 450);
+INSERT INTO products (product_name, department, price, stock_quantity) VALUES ('Rustic Gray Dining Set', 'Home and Kitchen', 824.99, 60);
 
 SELECT * FROM bamazon.products;
--- customer.js
+++ customer.js
@@ -1,8 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var http = require('http');
 
-// var server = http.createServer(handleRequest);
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -12,13 +10,6 @@ var connection = mysql.createConnection({
 });
 
 
-
-///////////////
-// FUNCTIONS //
-///////////////
-
-
-// Connects to database and console logs connection
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
@@ -26,13 +17,12 @@ connection.connect(function(err) {
 })
 
 
-// Starts Bamazon 
 var start = function() {
     connection.query('SELECT * FROM products', function(err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log('');
-            console.log('Department Name: ' + res[i].departmentName);
-            console.log('Item Id: ' + res[i].itemID + '|' + ' Product: ' + res[i].productName + '|' + ' Price: $' + res[i].price + '|' + ' In Stock: ' + res[i].stockQuantity);
+            console.log('Department Name: ' + res[i].department);
+            console.log('Item Id: ' + res[i].item_id + '|' + ' Product: ' + res[i].product_name + '|' + ' Price: $' + res[i].price + '|' + ' In Stock: ' + res[i].stock_quantity);
             console.log('-----------------------------------------------------------------------');
         }
         purchaseItem();
@@ -40,10 +30,9 @@ var start = function() {
 
 }
 
-// Function to purchase an item.
 var purchaseItem = function() {
     inquirer.prompt([{
-        name: "itemID",
+        name: "item_id",
         type: "input",
         message: "What item would you like to purchase? Select using ID.",
         validate: function(value) {
@@ -55,7 +44,7 @@ var purchaseItem = function() {
         }
 
     }, {
-        name: "stockQuantity",
+        name: "stock_quantity",
         type: "input",
         message: "How many would you like to purchase?",
         validate: function(value) {
@@ -67,20 +56,20 @@ var purchaseItem = function() {
         }
 
     }]).then(function(answer) {
-        connection.query('SELECT * FROM products WHERE itemID = ?', [answer.itemID], function(err, res) {
+        connection.query('SELECT * FROM products WHERE item_id = ?', [answer.item_id], function(err, res) {
             console.log(res);
-            if (answer.stockQuantity > res[0].stockQuantity) {
+            if (answer.stock_quantity > res[0].stock_quantity) {
                 console.log('Not enough in stock');
                 console.log('Go on, git!');
                 quitBamazon();
             } else {
-                priceTotal = res[0].price * answer.stockQuantity;
-                currentDepartment = res[0].departmentName;
+                priceTotal = res[0].price * answer.stock_quantity;
+                currentDepartment = res[0].department;
                 console.log('Your Total Amount is $' + priceTotal);
                 console.log('Thanks for your order');
                 console.log('');
-                var math = res[0].stockQuantity - parseInt(answer.stockQuantity);
-                connection.query('UPDATE products SET stockQuantity= ' + math + ' WHERE itemID =' + answer.itemID);
+                var math = res[0].stock_quantity - parseInt(answer.stock_quantity);
+                connection.query('UPDATE products SET stock_quantity= ' + math + ' WHERE item_id =' + answer.item_id);
                 quitBamazon();
             }
         });
@@ -88,8 +77,6 @@ var purchaseItem = function() {
 
 };
 
-
-// Function to quit Bamazon
 var quitBamazon = function() {
     connection.end();
 }
